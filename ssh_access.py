8773c0cb75725dc9fb7d8772f8fb86068f3f1bb7
tlangdun <tenzinlangdun@gmail.com>
2022-11-16 17:25:24 +0100

    updated ssh

--- ssh_access.py
+++ ssh_access.py
@@ -1,4 +1,4 @@
-import paramiko
+import paramiko, re, time
 
 command = "df"
 
@@ -8,10 +8,20 @@ command = "df"
 host = "192.168.123.12"
 username = "unitree"
 password = "123"
+command = "sudo ./walk.sh"
 
 client = paramiko.client.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect(host, username=username, password=password)
+
+channel = client.invoke_shell()
+channel.send(command)
+# wait for prompt
+while not re.search(".*\[sudo\].*",channel.recv(1024)): time.sleep(1)
+channel.send("%s\n" % password)
+
+'''
 _stdin, _stdout,_stderr = client.exec_command("df")
 print(_stdout.read().decode())
 client.close()
+'''
